d2dd6c2999e9a962f1bc5c9ff167bcce85362d1b
Eray Ferah <ery.ferah@gmail.com>
2022-01-31 00:22:20 +0100

    Graded Quiz Week 3 - fixed

--- R/Week_3/Graded_Quiz_3.R
+++ R/Week_3/Graded_Quiz_3.R
@@ -1,17 +1,67 @@
+
+# Graded Quiz 3 ----
+
 library(quantmod)
 
-# Exercise ----
+# Japanese Yen and US Dollars
 
-getSymbols("GOLDPMGBD228NLBM",src="FRED")
-gold <- na.omit(GOLDPMGBD228NLBM)
-gold <- gold["1979-12-31/2017-12-31"]
-names(gold) <- "TR"
-# Next, we calculated its daily log returns:
-logret <- diff(log(gold))[-1]
-# Assuming that daily log returns are normally distributed, we used the following R commands to
-# estimate the two parameters of the normal distribution: mean and standard deviation.
-round(mean(logret), 6)
-round(sd(logret), 6)
+getSymbols("DEXJPUS", src="FRED")
+JPY <- na.omit(DEXJPUS)
+JPY <- JPY["1979-12-31/2017-12-31"]
+JPY <- 1/JPY
+names(JPY) <- "TR"
+
+# We then ran the following sequence of R commands.
+# First, run the following R commands to see that the first observation is an “NA”:
+logret <- diff(log(JPY))
+head(logret,3)
+
+# Second, run the following R commend to see that the “NA” in the first observation is removed:
+logret <- diff(log(JPY))[-1]
+
+# In the lectures, we used the “moments” package in R to estimate its skewness, kurtosis, and to
+# perform the Jarque-Bera test of normality.
+# Skewness: The coefficient of skewness is estimated using the “skewness” function in the “moments”
+# package in R.
+library(moments)
+
+rvec <- as.vector(logret)
+
+round(skewness(rvec),2)
+
+# Kurtosis: The coefficient of kurtosis is estimated using the “kurtosis” function in the “moments”
+# package in R.
+library(moments)
+
+rvec <- as.vector(logret)
+
+round(kurtosis(rvec),2)
+
+# Jarque-Bera test of normality: The JB test of normality is performed using the “jarque.test” function in
+# the “moments” package in R.
+library(moments)
+
+rvec <- as.vector(logret)
+
+jarque.test(rvec)
+
+# In the lectures, we used the “moments” package in R to estimate the parameters of the scaled
+# student-t distribution:
+library(MASS)
+rvec <- as.vector(logret)
+t.fit <- fitdistr(rvec, "t")
+round(t.fit$estimate,6)
+# Using the estimated parameters, we estimated the VaR and ES at the 95% confidence level:
+alpha <- 0.01
+RNGkind(sample.kind = "Rounding")
+
+set.seed(123789)
+library(metRology)
+rvec <- rt.scaled(100000,mean=t.fit$estimate[1],sd=t.fit$estimate[2],df=t.fit$estimate[3])
+VaR <- quantile(rvec,alpha)
+ES <- mean(rvec[rvec<VaR])
+round(VaR,6)
+round(ES,6)
 
 # In the lectures, we used 3 simulation methods to find the VaR and ES at the 95% confidence
 # level for a 10-day horizon.
@@ -25,7 +75,7 @@ round(t.fit$estimate,6)
 # To estimate the VaR and ES at the 95% confidence interval for a 10-day horizon, we simulated
 # ten 1-day outcomes and add them up. Repeat 100,000 times. Then calculate the VaR and ES for
 # these 100,000 outcomes.
-alpha <- 0.05
+alpha <- 0.01
 RNGkind(sample.kind = "Rounding")
 set.seed(123789)
 rvec <- rep(0,100000)
@@ -42,7 +92,7 @@ round(ES_1,6)
 #   Instead of simulating from the scaled student-t distribution, the second simulation method
 # randomly samples ten 1-day observations from the empirical distribution and add them up.
 # Repeat 100,000 times. Then calculate the VaR and ES for these 100,000 outcomes.
-alpha <- 0.05
+alpha <- 0.01
 RNGkind(sample.kind = "Rounding")
 set.seed(123789)
 rvec <- rep(0,100000)
@@ -58,7 +108,7 @@ round(ES_2,6)
 #   The third simulation method also samples from the empirical distribution, but draws blocks of
 # ten consecutive 1-day outcomes and add them up. Repeat 100,000 times. Then calculate the
 # VaR and ES for these 100,000 outcomes.
-alpha <- 0.05
+alpha <- 0.01
 RNGkind(sample.kind = "Rounding")
 set.seed(123789)
 rdat <- as.vector(logret)
